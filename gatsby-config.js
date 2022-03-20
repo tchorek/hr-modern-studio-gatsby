4390c3d22cbba8065293c3008bd88b26f838b23a
Tchorek <michal_tch@wp.pl>
2022-03-20 23:30:08 +0100

    add homepage media

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 const gatsbyRequiredRules = path.join(
   process.cwd(),
-  "node_modules",
-  "gatsby",
-  "dist",
-  "utils",
-  "eslint-rules"
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules'
 );
 
 module.exports = {
@@ -15,53 +15,53 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-eslint",
+      resolve: 'gatsby-plugin-eslint',
       options: {
         rulePaths: [gatsbyRequiredRules],
-        stages: ["develop"],
-        extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', 'bower_components', '.cache', 'public'],
       },
     },
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "NR4fv4GTXfr2VdYx_JiUA_FqdaRluwlI_-qE6mZ2Ut4",
-        spaceId: "l4v38sfsi8tx",
+        accessToken: 'NR4fv4GTXfr2VdYx_JiUA_FqdaRluwlI_-qE6mZ2Ut4',
+        spaceId: 'l4v38sfsi8tx',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/assets/images/icon.png",
+        icon: 'src/assets/icons/favicon.png',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/assets/images/",
+        name: 'images',
+        path: './src/assets/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-plugin-root-import",
+      resolve: 'gatsby-plugin-root-import',
       options: {
-        assets: path.join(__dirname, "src", "assets"),
-        components: path.join(__dirname, "src", "components"),
+        assets: path.join(__dirname, 'src', 'assets'),
+        components: path.join(__dirname, 'src', 'components'),
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`montserrat\:300,500,700`, `cormorant garamond\:500`],
-        display: "swap",
+        display: 'swap',
       },
     },
   ],
